e5422eeae028b00712a614204d68254369d66f32
aijunhao <1172949809@qq.com>
2019-05-11 19:21:09 +0800

    添加cors跨域请求

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var bodyParser = require('body-parser')
-// var cors = require('cors')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 
 var app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -21,13 +22,6 @@ app.use(bodyParser.json())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-// app.use(
-//   cors({
-//     origin: ['http://0.0.0.0:8000'], // 允许访问域，vue 的ip端口
-//     methods: ['GET', 'POST'], // 允许 get 和 post
-//     allowedHeaders: ['Content-Type', 'Authorization'] // 允许这两种请求头
-//   })
-// )
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
